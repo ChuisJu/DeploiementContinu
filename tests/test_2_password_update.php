4059d2530657dcfbf4ea4a0c66daf591e436ba05
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2023-04-07 23:53:54 +0200

    Add files via upload

--- tests/test_2_password_update.php
+++ tests/test_2_password_update.php
@@ -8,7 +8,7 @@ $ret10 = update_password("NotAUsername", "password", "newpassword");
 $ret11 = update_password("username", "wrongpassword", "newpassword");
 $ret12 = update_password("username", "password", "");
 
-if(($re9 == 2) && ($ret10 == 3) && ($ret11 == 1) && ($ret12 == 0)){
+if(($ret9 == 2) && ($ret10 == 3) && ($ret11 == 1) && ($ret12 == 0)){
     printf("true");
     echo "test_2_password_update.php => true" >> "../rapport/hash_commit";
 
